6dfcda6d1b907c39b6ab43d5e9fb08c15edbdbb5
Kietnehi <149951917+Kietnehi@users.noreply.github.com>
2026-02-02 16:32:35 +0700

    KAGGLE GPU

--- Text-To-Speech-VietNamese/pyttsx3-tts.py
+++ Text-To-Speech-VietNamese/pyttsx3-tts.py
@@ -1,6 +1,19 @@
 import pyttsx3
 
 engine = pyttsx3.init()
-engine.setProperty('voice', 'vi') # Phải đảm bảo máy bạn đã cài gói tiếng Việt
-engine.say("Chào bạn, tôi đang nói offline.")
-engine.runAndWait()
+voices = engine.getProperty('voices')
+
+found_vi = False
+for voice in voices:
+    # Tìm giọng nói có ngôn ngữ Việt Nam
+    if "vietnam" in voice.name.lower() or "vi-vn" in voice.id.lower():
+        engine.setProperty('voice', voice.id)
+        found_vi = True
+        break
+
+if not found_vi:
+    print("Cảnh báo: Hệ thống chưa cài hoặc chưa tải xong giọng Tiếng Việt!")
+else:
+    engine.setProperty('rate', 150)
+    engine.say("Chào bạn, tôi đã tìm thấy giọng tiếng Việt.")
+    engine.runAndWait()
